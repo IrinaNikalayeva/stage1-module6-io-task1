685b952d53ba6f5f3eb8c1e68643005f6d48fe14
Iryna Nikalayeva <iryna_nikalayeva@epam.com>
2023-02-26 12:29:47 +0100

    performed profile task

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,38 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Map<String, String> finalMap = readData(file);
+        return new Profile(finalMap.get("Name"), Integer.parseInt(finalMap.get("Age")), finalMap.get("Email"),
+                           Long.parseLong(finalMap.get("Phone")));
+    }
+
+    public Map<String, String> readData(File file) {
+        try  {
+            Map<String, String> resultMap = new HashMap<>();
+            String result = Files.readString(file.toPath());
+            String[] keyValuePairs = result.split("\n");
+            for (String pair : keyValuePairs) {
+                String[] keyValue = pair.split(":");
+                if (keyValue.length == 2) {
+                    String key = keyValue[0];
+                    String value = keyValue[1].trim();
+                    resultMap.put(key, value);
+                }
+            }
+            System.out.println(resultMap);
+            return resultMap;
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
